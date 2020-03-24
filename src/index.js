bfe13a8d7ac7ceac3e734513026cc127bb317507
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-03-24 10:50:53 +0100

    :sparkles: Src > Index.js > Change Ceilings import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 import './style/main.styl'
 import * as THREE from 'three'
 import Walls from './javascript/Walls.js'
-import Ceilings from './javascript/ Ceilings.js'
+import Ceilings from './javascript/Ceilings.js'
 import Floors from './javascript/Floors.js'
 // import { TweenLite } from 'gsap/all'
 
